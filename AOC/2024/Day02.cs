b5c8a1d6a9578252e23a3f62745116eb153203ec
Doenja <45529866+Doenja@users.noreply.github.com>
2024-12-02 20:43:04 +0100

    Day 2, Part 1 & 2

---
+++ AOC/2024/Day02.cs
@@ -0,0 +1,98 @@
+﻿using System.ComponentModel;
+using AdventOfCodeSupport;
+
+namespace AOC._2024;
+
+public class Day02 : AdventBase
+{
+    protected override object InternalPart1()
+    {
+        int answer = 0;
+        int length = Input.Lines.Length;
+
+        foreach (string line in Input.Lines) 
+        {
+            var numbers = line.Split(' ')?.Select(Int32.Parse)?.ToList();
+
+            if(numbers != null)
+            { 
+                if(isListSafe(numbers)) answer++;
+            }
+        }
+        return answer;
+    }
+
+    protected override object InternalPart2()
+    {
+        int answer = 0;
+        int length = Input.Lines.Length;
+
+        foreach (string line in Input.Lines)
+        {
+            var numbers = line.Split(' ')?.Select(Int32.Parse)?.ToList();
+
+            if (numbers != null)
+            {
+                if (isListSafe(numbers))
+                {
+                    answer++;
+                } else
+                {
+                    bool isSafe = false;
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        var tempList = new List<int>(numbers);
+                        tempList.RemoveAt(i);
+                        if(isListSafe(tempList))
+                        {
+                            isSafe = true;
+                            break;
+                        }
+                    }
+                    if (isSafe) answer++;
+                };
+            }
+        }
+        return answer;
+    }
+
+
+    private bool isValidDifference(int n1, int n2)
+    {
+        return  n1 != n2 && Math.Abs(n1 - n2) < 4;
+    }
+
+    private bool isIncreasing(int n1, int n2)
+    {
+        return n2 > n1;
+    }
+
+    private bool isListSafe(List<int> numbers)
+    {
+        bool safe = true;
+        bool? increasing = null;
+
+        for (int i = 1; i < numbers.Count(); i++)
+        {
+            int nr1 = numbers[i - 1];
+            int nr2 = numbers[i];
+
+            if (!isValidDifference(nr1, nr2))
+            {
+                safe = false;
+                break;
+            }
+
+            if (increasing == null)
+            {
+                increasing = isIncreasing(nr1, nr2);
+            }
+            else if (increasing != isIncreasing(nr1, nr2))
+            {
+                safe = false;
+                break;
+            }
+        }
+        return safe;
+    }
+}
---
+++ AOCTest/Test02.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using AdventOfCodeSupport;
+using AdventOfCodeSupport.Testing;
+
+namespace AOC._2024;
+public class Test02
+{
+    private readonly AdventSolutions _solutions;
+
+    public Test02()
+    {
+        Assembly.Load("AOC");
+        _solutions = new AdventSolutions();
+    }
+
+    [Fact]
+    public void Part01()
+    {
+        var day = _solutions.GetDay(2024, 2);
+        day.SetTestInput("7 6 4 2 1\r\n1 2 7 8 9\r\n9 7 6 2 1\r\n1 3 2 4 5\r\n8 6 4 4 1\r\n1 3 6 7 9");
+        Assert.Equal("2", day.Part1Answer);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var day = _solutions.GetDay(2024, 2);
+        day.SetTestInput("7 6 4 2 1\r\n1 2 7 8 9\r\n9 7 6 2 1\r\n1 3 2 4 5\r\n8 6 4 4 1\r\n1 3 6 7 9");
+        Assert.Equal("4", day.Part2Answer);
+    }
+}
