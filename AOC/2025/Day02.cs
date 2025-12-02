4c2dea0f65b84158c5080831c3bb871e0f5077fe
Doenja <45529866+Doenja@users.noreply.github.com>
2025-12-02 16:02:24 +0100

    2025 Day 02 Part 1

---
+++ AOC/2025/Day02.cs
@@ -0,0 +1,66 @@
+﻿using AdventOfCodeSupport;
+using System.Numerics;
+
+namespace AOC._2025
+{
+    public class Day02 : AdventBase
+    {
+        protected override object InternalPart1()
+        {
+            BigInteger answer = 0;
+            var ranges = Input.Lines[0].Split(',');
+
+            foreach (var range in ranges)
+            {
+                var parts = range.Split('-');
+
+                string startDigits = parts[0];
+                string endDigits = parts[1];
+
+                long startLong = long.Parse(parts[0]);
+                long endLong = long.Parse(parts[1]);
+
+                for (int i = startDigits.Length; i < endDigits.Length + 1; i++)
+                {
+                    // Only numbers with an even amount of digits can be a sequence repeated twice
+                    if(i % 2 == 0)
+                    {
+                        // Generate a number with half the amount of digits i,
+                        // Example: i=6, generatedNumberPart = 100 (3 digits)
+                        var generatedNumberPart = (long)Math.Pow(10, i / 2 - 1);
+
+                        while (true)
+                        {
+                            var generated = MakeRepeatedSequence(generatedNumberPart);
+                            if (generated > endLong)
+                            {
+                                break;
+                            }
+                            if(generated >= startLong)
+                            {
+                                answer += generated;
+                            }
+
+                            generatedNumberPart++;
+                        }
+                    }
+                }
+
+            }
+            return answer;
+        }
+
+        protected override object InternalPart2()
+        {
+            int answer = 0;
+
+            return answer;
+        }
+
+        public static BigInteger MakeRepeatedSequence(long nr)
+        {
+            int digits = nr.ToString().Length;
+            return nr * (long)Math.Pow(10, digits) + nr;
+        }
+    }
+}
---
+++ AOCTest/2025/Test02.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using AdventOfCodeSupport;
+using AdventOfCodeSupport.Testing;
+using AOC._2025;
+
+namespace AOC._2025;
+public class Test02
+{
+    private readonly AdventSolutions _solutions;
+
+    public Test02()
+    {
+        Assembly.Load("AOC");
+        _solutions = new AdventSolutions();
+    }
+
+    [Fact]
+    public void Part01()
+    {
+        var day = _solutions.GetDay(2025, 2);
+        day.SetTestInput("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124");
+        Assert.Equal("1227775554", day.Part1Answer);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var day = _solutions.GetDay(2025, 2);
+        day.SetTestInput("");
+        Assert.Equal("", day.Part2Answer);
+    }
+
+    [Fact]
+    public static void TestRepeatSequence()
+    {
+        var output = Day02.MakeRepeatedSequence(12534);
+        Assert.Equal(1253412534, output);
+    }
+}
