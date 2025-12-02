1e016f931951b3d6a339ab0f5a2709abaeacfcf1
Doenja <45529866+Doenja@users.noreply.github.com>
2025-12-02 16:54:37 +0100

    2025 Day02 Part 2

--- AOC/2025/Day02.cs
+++ AOC/2025/Day02.cs
@@ -25,13 +25,12 @@ namespace AOC._2025
                     // Only numbers with an even amount of digits can be a sequence repeated twice
                     if(i % 2 == 0)
                     {
-                        // Generate a number with half the amount of digits i,
-                        // Example: i=6, generatedNumberPart = 100 (3 digits)
-                        var generatedNumberPart = (long)Math.Pow(10, i / 2 - 1);
+                        // Generate a number with half the amount of digits i
+                        var generatedNumberPart = GenerateNumberPart(i / 2);
 
                         while (true)
                         {
-                            var generated = MakeRepeatedSequence(generatedNumberPart);
+                            var generated = MakeRepeatedSequence(generatedNumberPart, 2);
                             if (generated > endLong)
                             {
                                 break;
@@ -52,15 +51,93 @@ namespace AOC._2025
 
         protected override object InternalPart2()
         {
-            int answer = 0;
+            BigInteger answer = 0;
+            var ranges = Input.Lines[0].Split(',');
 
+            foreach (var range in ranges)
+            {
+                var parts = range.Split('-');
+
+                string startDigits = parts[0];
+                string endDigits = parts[1];
+
+                long startLong = long.Parse(parts[0]);
+                long endLong = long.Parse(parts[1]);
+
+                var uniqueInvalidIds = new HashSet<BigInteger>();
+
+                for (int i = startDigits.Length; i < endDigits.Length + 1; i++)
+                {
+                    var divisors = GetDivisors(i);
+
+
+                    foreach (var divisor in divisors)
+                    {
+                        if (divisor == 1) continue;
+
+                        var generatedNumberPart = GenerateNumberPart(i / divisor);
+
+                        while (true)
+                        {
+                            var generated = MakeRepeatedSequence(generatedNumberPart, divisor);
+                            if (generated > endLong)
+                            {
+                                break;
+                            }
+                            if (generated >= startLong)
+                            {
+                                uniqueInvalidIds.Add(generated); 
+                            }
+
+                            generatedNumberPart++;
+                        }
+
+                    }
+                }
+
+                foreach (var invalidId in uniqueInvalidIds)
+                {
+                    answer += invalidId;
+                }
+            }
+            
             return answer;
         }
 
-        public static BigInteger MakeRepeatedSequence(long nr)
+
+        // Example: numberOfDigits=6, returns 100000 
+        public static long GenerateNumberPart(int numberOfDigits)
+        {
+            return (long)Math.Pow(10, numberOfDigits - 1);
+        }
+
+        public static BigInteger MakeRepeatedSequence(long nr, int times)
         {
             int digits = nr.ToString().Length;
-            return nr * (long)Math.Pow(10, digits) + nr;
+            BigInteger result = 0;
+            BigInteger factor = BigInteger.Pow(10, digits);
+
+            for (int i = 0; i < times; i++)
+            {
+                result = result * factor + nr;
+            }
+
+            return result;
+        }
+
+        public static List<int> GetDivisors(int n)
+        {
+            var divisors = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+
+            return divisors;
         }
     }
 }
--- AOCTest/2025/Test02.cs
+++ AOCTest/2025/Test02.cs
@@ -26,14 +26,35 @@ public class Test02
     public void Part02()
     {
         var day = _solutions.GetDay(2025, 2);
-        day.SetTestInput("");
-        Assert.Equal("", day.Part2Answer);
+        day.SetTestInput("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124");
+        Assert.Equal("4174379265", day.Part2Answer);
+    }
+
+    [Fact]
+    public static void TestGenerateNumberPart()
+    {
+        Assert.Equal(10000, Day02.GenerateNumberPart(5));
+        Assert.Equal(1, Day02.GenerateNumberPart(1));
+        Assert.Equal(10, Day02.GenerateNumberPart(2));
     }
 
     [Fact]
     public static void TestRepeatSequence()
     {
-        var output = Day02.MakeRepeatedSequence(12534);
+        var output = Day02.MakeRepeatedSequence(12534, 2);
         Assert.Equal(1253412534, output);
+
+        var output2 = Day02.MakeRepeatedSequence(32, 3);
+        Assert.Equal(323232, output2);
+
+        var output3 = Day02.MakeRepeatedSequence(7, 12);
+        Assert.Equal(777777777777, output3);
+    }
+
+    [Fact]
+    public static void TestGetDivisors()
+    {
+        Assert.Equal(new List<int>{ 1, 2, 3, 6 }, Day02.GetDivisors(6));
+        Assert.Equal(new List<int> { 1, 7 }, Day02.GetDivisors(7));
     }
 }
