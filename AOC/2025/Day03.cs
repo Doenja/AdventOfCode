90432142b9d2ae96d4f680f5e742298506431e0d
Doenja <45529866+Doenja@users.noreply.github.com>
2025-12-03 16:22:19 +0100

    2025 Day03 Part 1

---
+++ AOC/2025/Day03.cs
@@ -0,0 +1,63 @@
+﻿using AdventOfCodeSupport;
+using System.Numerics;
+
+namespace AOC._2025
+{
+    public class Day03 : AdventBase
+    {
+        protected override object InternalPart1()
+        {
+            int answer = 0;
+
+            foreach(var line in Input.Lines)
+            {
+                int firstNr = 0;
+                int startingIndex = 0;
+                int secondNr = 0;
+        
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    var nr = int.Parse(line[i].ToString());
+                    if(i == line.Length -1)
+                    {
+                        secondNr = nr;
+                        break;
+                    }
+
+                    if (nr > firstNr)
+                    {
+                        firstNr = nr;
+                        startingIndex =  i;
+                    }
+                }
+
+                for (int i = startingIndex; i < line.Length; i++)
+                {
+                    if(i == startingIndex)
+                    {
+                        continue;
+                    }
+                    var nr = int.Parse(line[i].ToString());
+                    if (nr > secondNr)
+                    {
+                        secondNr = nr;
+                    }
+                }
+
+                answer += firstNr * 10 + secondNr;
+
+            }
+            
+            return answer;
+        }
+
+        protected override object InternalPart2()
+        {
+            int answer = 0;
+                       
+            return answer;
+        }
+
+    }
+}
---
+++ AOCTest/2025/Test03.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using AdventOfCodeSupport;
+using AdventOfCodeSupport.Testing;
+using AOC._2025;
+
+namespace AOC._2025;
+public class Test03
+{
+    private readonly AdventSolutions _solutions;
+
+    public Test03()
+    {
+        Assembly.Load("AOC");
+        _solutions = new AdventSolutions();
+    }
+
+    [Fact]
+    public void Part01()
+    {
+        var day = _solutions.GetDay(2025, 3);
+        day.SetTestInput("987654321111111\r\n811111111111119\r\n234234234234278\r\n818181911112111\r\n");
+        Assert.Equal("357", day.Part1Answer);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var day = _solutions.GetDay(2025, 3);
+        day.SetTestInput("987654321111111\r\n811111111111119\r\n234234234234278\r\n818181911112111\r\n");
+        Assert.Equal("", day.Part2Answer);
+    }
+
+  
+}
