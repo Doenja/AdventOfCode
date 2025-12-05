e29d48479376fc084d31cdce84a35004fa1554ba
Doenja <45529866+Doenja@users.noreply.github.com>
2025-12-05 16:59:10 +0100

    2025 Day05 Part 1

---
+++ AOC/2025/Day05.cs
@@ -0,0 +1,85 @@
+﻿using AdventOfCodeSupport;
+
+namespace AOC._2025
+{
+    public class Day05 : AdventBase
+    {
+        protected override object InternalPart1()
+        {
+            long answer = 0;
+
+            var ranges = new List<(long start, long end)>();
+
+            var processingRanges = true;
+            foreach (var line in Input.Lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    processingRanges = false;
+                    continue;
+                }
+
+                if (processingRanges)
+                {
+                    var start = long.Parse(line.Split('-')[0]);
+                    var end = long.Parse(line.Split('-')[1]);
+
+                    var rangesToRemove = new List<(long start, long end)>();
+                    foreach (var range in ranges)
+                    {
+                        if (RangesOverlap(start, end, range.start, range.end))
+                        {
+                            rangesToRemove.Add(range);
+                            start = Math.Min(start, range.start);
+                            end = Math.Max(end, range.end);
+                        } 
+                    }
+
+                    ranges.Add((start, end));
+
+                    foreach (var range in rangesToRemove)
+                    {
+                        ranges.Remove(range);
+                    }
+                }
+                else
+                {
+                    // Check if ingredient is in ranges
+                    var ingredient = long.Parse(line);
+                    foreach (var range in ranges)
+                    {
+                        if (IsInRange(ingredient, range.start, range.end))
+                        {
+                            answer++;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            return answer;
+        }
+
+        protected override object InternalPart2()
+        {
+            int answer = 0;
+            var lines = Input.Lines;
+
+          
+
+            return answer;
+        }
+
+      
+        public static bool RangesOverlap(long start1, long end1, long start2, long end2)
+        {
+            return start1 <= end2 && start2 <= end1;
+        }
+
+        public static bool IsInRange(long number, long rangeStart, long rangeEnd)
+        {
+            return number >= rangeStart && number <= rangeEnd;
+        }
+
+    }
+}
---
+++ AOCTest/2025/Test05.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using AdventOfCodeSupport;
+using AdventOfCodeSupport.Testing;
+using AOC._2025;
+
+namespace AOC._2025;
+public class Test05
+{
+    private readonly AdventSolutions _solutions;
+
+    public Test05()
+    {
+        Assembly.Load("AOC");
+        _solutions = new AdventSolutions();
+    }
+
+    [Fact]
+    public void Part01()
+    {
+        var day = _solutions.GetDay(2025, 5);
+        day.SetTestInput("3-5\r\n10-14\r\n16-20\r\n12-18\r\n\r\n1\r\n5\r\n8\r\n11\r\n17\r\n32");
+        Assert.Equal("3", day.Part1Answer);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var day = _solutions.GetDay(2025, 5);
+        day.SetTestInput("3-5\r\n10-14\r\n16-20\r\n12-18\r\n\r\n1\r\n5\r\n8\r\n11\r\n17\r\n32");
+        Assert.Equal("", day.Part2Answer);
+    }
+
+  
+}
