f2e5cce18b5d2e01edf314af1c39d4a5d59f4196
Doenja <45529866+Doenja@users.noreply.github.com>
2025-12-06 10:58:48 +0100

    2025 Day06 Part 1

---
+++ AOC/2025/Day06.cs
@@ -0,0 +1,68 @@
+﻿using AdventOfCodeSupport;
+using System.Text.RegularExpressions;
+
+namespace AOC._2025
+{
+    public class Day06 : AdventBase
+    {
+        protected override object InternalPart1()
+        {
+            long answer = 0;
+            Dictionary<int, List<int>> numberCols = new();
+
+            foreach (var line in Input.Lines)
+            {
+                var split = Regex.Split(line.Trim(), @"\s+");
+                for (int i = 0; i < split.Length; i++)
+                {
+                    if (split[i] == "*")
+                    {
+                        answer += MultiplyAllNumbers(numberCols[i]);
+                        continue;
+                    }
+
+                    if (split[i] == "+")
+                    {
+                        answer += AddAllNumbers(numberCols[i]);
+                        continue;
+                    }
+
+                    if (int.TryParse(split[i], out int nr))
+                    {
+                        if(!numberCols.ContainsKey(i))
+                        {
+                            numberCols[i] = new List<int>();
+                        }
+                        numberCols[i].Add(nr);
+                    }
+                } 
+            }
+
+            return answer;
+        }
+
+        protected override object InternalPart2()
+        {
+            long answer = 0;
+
+            foreach (var line in Input.Lines)
+            {
+            }
+
+            return answer;
+        }
+
+
+        public long MultiplyAllNumbers(List<int> numbers)
+        {
+            return numbers.Aggregate(1L, (acc, val) => acc * val);
+        }
+
+        public long AddAllNumbers(List<int> numbers)
+        {
+            return numbers.Aggregate(0L, (acc, val) => acc + val);
+        }
+
+
+    }
+}
---
+++ AOCTest/2025/Test06.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using AdventOfCodeSupport;
+using AdventOfCodeSupport.Testing;
+using AOC._2025;
+
+namespace AOC._2025;
+public class Test06
+{
+    private readonly AdventSolutions _solutions;
+
+    public Test06()
+    {
+        Assembly.Load("AOC");
+        _solutions = new AdventSolutions();
+    }
+
+    [Fact]
+    public void Part01()
+    {
+        var day = _solutions.GetDay(2025, 6);
+        day.SetTestInput("123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   +");
+        Assert.Equal("4277556", day.Part1Answer);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var day = _solutions.GetDay(2025, 6);
+        day.SetTestInput("123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   +");
+        Assert.Equal("", day.Part2Answer);
+    }
+
+  
+}
