8a01ed5aab01acb1daf54ff9d2f1905b97616952
Doenja <45529866+Doenja@users.noreply.github.com>
2025-12-07 13:53:18 +0100

    2025 Day07 Part 1

---
+++ AOC/2025/Day07.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCodeSupport;
+using System.Text.RegularExpressions;
+
+namespace AOC._2025
+{
+    public class Day07 : AdventBase
+    {
+        protected override object InternalPart1()
+        {
+            int answer = 0;
+            var beamIndices = new HashSet<int>();
+
+            foreach (var line in Input.Lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    char ch = line[i];
+                    if(ch == 'S')
+                    {
+                        beamIndices.Add(i);
+                    }
+
+                    if (ch == '^' && beamIndices.Contains(i))
+                    {
+                        beamIndices.Remove(i); // Beam does not continue downwards for this index
+                        if (i > 0) beamIndices.Add(i - 1); // It continues to the left
+                        if (i < line.Length - 1) beamIndices.Add(i + 1); // It continues to the right
+                        answer++; // Count the split
+                    }
+                }            
+            }
+            return answer;
+        }
+
+        protected override object InternalPart2()
+        {
+            long answer = 0;
+
+            foreach (var line in Input.Lines)
+            {
+
+            }
+
+            return answer;
+        }
+
+    }
+}
---
+++ AOCTest/2025/Test07.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using AdventOfCodeSupport;
+using AdventOfCodeSupport.Testing;
+using AOC._2025;
+
+namespace AOC._2025;
+
+public class Test07
+{
+    private readonly AdventSolutions _solutions;
+
+    public Test07()
+    {
+        Assembly.Load("AOC");
+        _solutions = new AdventSolutions();
+    }
+
+    [Fact]
+    public void Part01()
+    {
+        var day = _solutions.GetDay(2025, 7);
+        day.SetTestInput(".......S.......\r\n...............\r\n.......^.......\r\n...............\r\n......^.^......\r\n...............\r\n.....^.^.^.....\r\n...............\r\n....^.^...^....\r\n...............\r\n...^.^...^.^...\r\n...............\r\n..^...^.....^..\r\n...............\r\n.^.^.^.^.^...^.\r\n...............");
+        Assert.Equal("21", day.Part1Answer);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var day = _solutions.GetDay(2025, 7);
+        day.SetTestInput("");
+        Assert.Equal("", day.Part2Answer);
+    }
+
+
+}
