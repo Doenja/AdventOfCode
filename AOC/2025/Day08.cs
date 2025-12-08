84092ee7df7e083dadec1503791b1c4ba45c6e85
Doenja <45529866+Doenja@users.noreply.github.com>
2025-12-08 15:38:07 +0100

    2025 Day08 Part 1

---
+++ AOC/2025/Day08.cs
@@ -0,0 +1,128 @@
+﻿using AdventOfCodeSupport;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace AOC._2025
+{
+    public class Day08 : AdventBase
+    {
+        protected override object InternalPart1()
+        {
+            int answer = 0;
+            
+            var points = new List<Point>();
+
+            // Parse input into points
+            foreach (var line in Input.Lines)
+            {
+                var split = line.Split(',');
+                int x = int.Parse(split[0]);
+                int y = int.Parse(split[1]);
+                int z = int.Parse(split[2]);
+                points.Add(new Point(x, y, z));
+            }
+
+            // Calculate distances between all points
+            var distances = new Dictionary<(Point, Point), double>();
+
+            foreach (var pointA in points)
+            {
+                foreach (var pointB in points)
+                {
+                    if(pointA.Equals(pointB) || distances.ContainsKey((pointA, pointB)) || distances.ContainsKey((pointB, pointA)))
+                    {
+                        continue;
+                    }
+                    double distance = EuclideanDistance(pointA, pointB);
+                    distances.Add((pointA, pointB), distance);
+                }
+            }
+
+            // Sort distances
+            distances = distances
+                .OrderBy(kv => kv.Value)
+                .ToDictionary(kv => kv.Key, kv => kv.Value);
+
+            // Connect the circuits
+            var circuits = new List<HashSet<Point>>();
+            var connect = 1000;
+
+            foreach (var distance in distances)
+            {
+                if(connect == 0) break;
+
+                var pointA = distance.Key.Item1;
+                var pointB = distance.Key.Item2;
+
+                var circuitA = circuits.FirstOrDefault(c => c.Contains(pointA));
+                var circuitB = circuits.FirstOrDefault(c => c.Contains(pointB));
+
+                if (circuitA == null && circuitB == null)
+                {
+                    circuits.Add(new HashSet<Point> { pointA, pointB });
+                }
+                else if (circuitA != null && circuitB == null)
+                {
+                    circuitA.Add(pointB);
+                }
+                else if (circuitA == null && circuitB != null)
+                {
+                    circuitB.Add(pointA);
+                }
+                else if (circuitA != null && circuitB != null && !circuitA.SetEquals(circuitB))
+                {
+                    circuitA.UnionWith(circuitB);
+                    circuits.Remove(circuitB);
+                }
+                connect--;
+                
+            }
+
+            answer = circuits
+                .Select(inner => inner.Count)
+                .OrderByDescending(count => count)
+                .Take(3)
+                .Aggregate(1, (prod, count) => prod * count);
+
+            return answer;
+        }
+
+        protected override object InternalPart2()
+        {
+            int answer = 0;
+
+            foreach (var line in Input.Lines)
+            {
+
+            }
+
+            return answer;
+        }
+
+        public struct Point
+        {
+            public int X;
+            public int Y;
+            public int Z;
+            public Point(int x, int y, int z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public override bool Equals([NotNullWhen(true)] object? obj)
+            {
+                return base.Equals(obj);
+            }
+
+            public override string ToString() => $"({X}, {Y}, {Z})";
+        }
+
+        public double EuclideanDistance(Point a, Point b)
+        {
+            double distance = (double)Math.Sqrt(Math.Pow(b.X - a.X, 2) + Math.Pow(b.Y - a.Y, 2) + Math.Pow(b.Z - a.Z, 2));
+            return Math.Round(distance, 2);
+        }
+    }
+}
---
+++ AOCTest/2025/Test08.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using AdventOfCodeSupport;
+
+namespace AOC._2025;
+using static AOC._2025.Day08;
+
+public class Test08
+{
+    private readonly AdventSolutions _solutions;
+
+    public Test08()
+    {
+        Assembly.Load("AOC");
+        _solutions = new AdventSolutions();
+    }
+
+    [Fact]
+    public void Part01()
+    {
+        var day = _solutions.GetDay(2025, 8);
+        day.SetTestInput("162,817,812\r\n57,618,57\r\n906,360,560\r\n592,479,940\r\n352,342,300\r\n466,668,158\r\n542,29,236\r\n431,825,988\r\n739,650,466\r\n52,470,668\r\n216,146,977\r\n819,987,18\r\n117,168,530\r\n805,96,715\r\n346,949,466\r\n970,615,88\r\n941,993,340\r\n862,61,35\r\n984,92,344\r\n425,690,689");
+        Assert.Equal("40", day.Part1Answer);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var day = _solutions.GetDay(2025, 8);
+        day.SetTestInput("162,817,812\r\n57,618,57\r\n906,360,560\r\n592,479,940\r\n352,342,300\r\n466,668,158\r\n542,29,236\r\n431,825,988\r\n739,650,466\r\n52,470,668\r\n216,146,977\r\n819,987,18\r\n117,168,530\r\n805,96,715\r\n346,949,466\r\n970,615,88\r\n941,993,340\r\n862,61,35\r\n984,92,344\r\n425,690,689");
+        Assert.Equal("", day.Part2Answer);
+    }
+
+    [Fact]
+    public void EuclideanDistance()
+    {
+        var day08 = new Day08();
+        var result = day08.EuclideanDistance(new Point(1,2,3), new Point(4,6,8));
+        Assert.Equal(7.07, result, precision: 3);
+    }
+
+
+}
