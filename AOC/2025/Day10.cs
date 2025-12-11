76506bb61d18940a6ddb8f0d5aca0186a10571f4
Doenja <45529866+Doenja@users.noreply.github.com>
2025-12-11 15:56:51 +0100

    2025 Day10 wip

---
+++ AOC/2025/Day10.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCodeSupport;
+using System.Text.RegularExpressions;
+
+namespace AOC._2025
+{
+    public class Day010 : AdventBase
+    {
+        protected override object InternalPart1()
+        {
+            int answer = 0;
+
+            foreach (var line in Input.Lines)
+            {
+                var m1 = Regex.Matches(line, @"\[(.*?)\]");
+                var diagram = m1[0].Groups[1].Value;
+                int bits = GetBits(diagram);
+
+                var m2 = Regex.Matches(line, @"\((.*?)\)");
+                var buttons = new List<List<int>>();
+                foreach (var m in m2)
+                {
+                    if (m == null) continue;
+                    var s = m.ToString();
+                    List<int> newButtons = s
+                        .Trim('(', ')')
+                        .Split(',')
+                        .Select(c => int.Parse(c.ToString()))
+                        .ToList();
+
+                    buttons.Add(newButtons);
+                }
+            }
+
+            return answer;
+        }
+
+
+        protected override object InternalPart2()
+        {
+            int answer = 0;
+
+            return answer;
+        }
+
+        public int GetBits(string s)
+        {
+            int value = 0;
+            foreach (var ch in s)
+            {
+                value <<= 1;
+                if (ch == '#')
+                    value |= 1;
+            }
+            return value;
+        }
+
+
+    }
+}
---
+++ AOCTest/2025/Test10.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCodeSupport;
+using System.Reflection;
+
+namespace AOC._2025;
+
+public class Test10
+{
+    private readonly AdventSolutions _solutions;
+
+    public Test10()
+    {
+        Assembly.Load("AOC");
+        _solutions = new AdventSolutions();
+    }
+
+    [Fact]
+    public void Part01()
+    {
+        var day = _solutions.GetDay(2025, 10);
+        day.SetTestInput("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\r\n[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\r\n[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}");
+        Assert.Equal("7", day.Part1Answer);
+    }
+
+    [Fact]
+    public void Part02()
+    {
+        var day = _solutions.GetDay(2025, 10);
+        day.SetTestInput("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\r\n[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\r\n[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}");
+        Assert.Equal("", day.Part2Answer);
+    }
+}
